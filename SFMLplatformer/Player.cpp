027187020c00e836f57179fef98d5437924a635b
Foxsia <foxsia29@gmail.com>
2026-05-07 12:02:46 +0200

    first animation

--- SFMLplatformer/Player.cpp
+++ SFMLplatformer/Player.cpp
@@ -1,6 +1,11 @@
 #include "Player.h"
 #include <iostream>
 
+void Player::initVariables()
+{
+	moving = false;
+}
+
 void Player::initTexture()
 {
 	if (!textureSheet.loadFromFile("assets/player.png"))
@@ -14,12 +19,20 @@ void Player::initSprite()
 	sprite.setTexture(textureSheet);
 	currentFrame = sf::IntRect(0, 0, 32, 32);
 	sprite.setTextureRect(currentFrame);
+	sprite.setScale(2.5f, 2.5f);
+}
+
+void Player::initAnimations()
+{
+	animationTimer.restart();
 }
 
 Player::Player()
 {
+	initVariables();
 	initTexture();
 	initSprite();
+	initAnimations();
 }
 
 Player::~Player()
@@ -28,28 +41,50 @@ Player::~Player()
 
 void Player::updateMovement()
 {
+	moving = false;
 	//change it to be outside of player(too many ifs)
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))//left
 	{
 		sprite.move(-1.f, 0.f);
+		moving = true;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))//right
 	{
 		sprite.move(1.f, 0.f);
+		moving = true;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))//top
 	{
 		sprite.move(0.f, -1.f);
+		moving = true;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))//down
 	{
 		sprite.move(0.f, 1.f);
+		moving = true;
+	}
+}
+
+void Player::updateAnimation()
+{
+	//seperate classes in the future
+	if (animationTimer.getElapsedTime().asSeconds() >= 0.2f)
+	{
+		if (!moving)
+		{
+			currentFrame.left += 32.f;
+			if (currentFrame.left >= 96.f) currentFrame.left = 0;
+		}
+
+		animationTimer.restart();
+		sprite.setTextureRect(currentFrame);
 	}
 }
 
 void Player::update()
 {
 	updateMovement();
+	updateAnimation();
 }
 
 void Player::render(sf::RenderTarget& target)
--- SFMLplatformer/Player.h
+++ SFMLplatformer/Player.h
@@ -7,18 +7,23 @@ class Player
 private:
 	sf::Sprite sprite;
 	sf::Texture textureSheet;
+	bool moving;
+	sf::Clock animationTimer;
 
 	//animation
 	sf::IntRect currentFrame;
 
+	void initVariables();
 	void initTexture();
 	void initSprite();
+	void initAnimations();
 public:
 	Player();
 	virtual ~Player();
 
 	//func
 	void updateMovement();
+	void updateAnimation();
 	void update();
 	void render(sf::RenderTarget& target);
 };
