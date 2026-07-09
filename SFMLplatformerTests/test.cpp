b24de8f10720cd110e87ef96b87a3c33e345b0b4
Foxsia <foxsia29@gmail.com>
2026-07-09 12:49:51 +0200

    configure test environment and add first test

--- SFMLplatformerTests/test.cpp
+++ SFMLplatformerTests/test.cpp
@@ -1,7 +1,16 @@
 #include "pch.h"
 #include <gtest/gtest.h>
+#include "ConfigManager.h"
+#include "Player.h"
 
-TEST(SampleTest, TrueIsTrue)
+TEST(PlayerHealth, DamageHalf)
 {
-    EXPECT_TRUE(true);
+    fp::ConfigManager::load("../SFMLplatformer/config/gameplay.json");
+
+    fp::Player player;
+
+    player.takeDamage(player.getMaxHealth() / 2.f);
+
+    EXPECT_EQ(player.getHealth(), 1);
+    EXPECT_TRUE(player.isAlive());
 }
